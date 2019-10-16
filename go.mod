d0d54573acddf686fb56f191c3b9a093b499b6b0
roy.wang <roy.wang@roiland.com>
2019-10-16 18:17:15 +0800

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module timer
 
 go 1.13
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/wiloon/pingd-log v0.0.0-20190908085316-06e9b816cb3e
+	github.com/wiloon/pingd-utils v0.0.0-20190908054416-c9c92226ca4f // indirect
+	go.etcd.io/bbolt v1.3.3
+	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
+)
--- timer.go
+++ timer.go
@@ -1,22 +1,52 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/wiloon/pingd-log/logconfig"
+	"go.etcd.io/bbolt"
 	"net/http"
 	"time"
 )
 
+const bucketTimer = "timer"
+
 func main() {
+	logconfig.Init()
 	// get system time
-	systemTime := time.Now().Format("2006-01-02T15:04:05Z07:00")
+	systemTime := time.Now()
+	systemTimeStr := systemTime.Format("2006-01-02T15:04:05Z07:00")
+	systemTimeStrDay := systemTime.Format("2006-01-02")
 	// get net time via http request
 	resp, _ := http.Get("http://www.ntsc.ac.cn")
-	foo := resp.Header.Get("Date")
+	netTime := resp.Header.Get("Date")
 
-	log.Println(systemTime)
-	log.Println(foo)
+	log.Infof("system time: %v", "net time: %v", systemTimeStr, netTime)
+
 	//save to db //基于文件的数据库
-	//todo, bbolt
+
+	db, err := bbolt.Open("/tmp/foo.db", 0666, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+
+	_ = db.Update(func(tx *bbolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucketTimer))
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = b.Put([]byte(systemTimeStrDay), []byte(systemTimeStr))
+		return err
+	})
+
+	db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(bucketTimer))
+		v := b.Get([]byte(systemTimeStrDay))
+		fmt.Printf("value: %s\n", v)
+		return nil
+	})
+
 	//search
 	//查询某一天最早的时间
 
